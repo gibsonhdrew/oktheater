fca0fc5fa9df0948b303e1faa2ea08b4860bb824
dhg <gibsonhdrew@gmail.com>
2017-11-08 14:23:19 -0500

    graphics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import logo from './images/logo.jpg';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className='oktHeader'>
-            <span className="oktTitle">Nature Theater of Oklahoma</span>
-            <ul className="mainNav">
-                <li>Home</li>
-                <li>We are OK</li>
-                <li>OK Theater</li>
-                <li>OK Radio</li>
-                <li>OK Books</li>
-            </ul>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Root>
+                    <Header/>
+                    <Main/>
+                </Root>
+            </Router>
+        );
+    }
 }
+
+const Root = (props) => (
+    <div style={{
+        
+    }} {...props} />
+)
+
+const Header = (props) => (
+    <div className='oktHeader' {...props}>
+        <Link to='/'>
+            <img src={logo} alt='logo' style={{
+                width: "25%"
+            }}></img>
+        </Link>
+        <ul className="mainNav">
+            <Link to='/we-are-ok'><li>We&#39;re OK</li></Link>
+            <Link to='/oktheater'><li>OK Theater</li></Link>
+            <Link to='/okvideo'><li>OK Video</li></Link>  
+            <Link to='/okradio'><li>OK Radio</li></Link>
+            <Link to='/okbooks'><li>OK Books</li></Link>
+        </ul>
+    </div>
+)
+
+const Main = (props) => (
+    <div> 
+        <Route path='/we-are-ok' component={Weareok}/>
+        <Route path='/oktheater' component={Oktheater}/>
+        <Route path='/okvideo' component={Okvideo}/>
+        <Route path='/okradio' component={Okradio}/>
+        <Route path='/okbooks' component={Okbooks}/>
+        <Route path='/contacts' component={Contacts}/>
+    </div>
+)
+
+const Weareok = (props) => (
+    <div {...props}>
+        <p>We are ok</p>
+        <Link to='/contacts'><p>..Contacts</p></Link>
+    </div>
+)
+
+const Oktheater = (props) => (
+    <div {...props}>
+        <p>Ok Theater</p>
+    </div>
+)
+
+const Okvideo = (props) => (
+    <div {...props}>
+        <p>Ok Video</p>
+    </div>
+)
+
+const Okradio = (props) => (
+    <div {...props}>
+        <p>Ok Radio</p>
+    </div>
+)
+
+const Okbooks = (props) => (
+    <div {...props}>
+        <p>Ok Books</p>
+    </div>
+)
+
+const Contacts = (props) => (
+    <div {...props}>
+        <p>Contacts</p>
+    </div>
+)
 
 export default App;
