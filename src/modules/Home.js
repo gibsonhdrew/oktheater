7bf657089760066747250398b225c8e130f21023
dhg <gibsonhdrew@gmail.com>
2017-11-30 14:03:06 -0500

    mobile home page

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import news from '../images/news.png';
+import hr3 from '../images/hr3.png';
+import mailinglist from '../images/mailinglist.png';
+import oktwitter from '../images/oktwitter.png';
 
 
 class Home extends Component {
@@ -13,14 +16,12 @@ class Home extends Component {
         var slugs = []
         var previousPosts = []
         var previousSlugs = []
-        var thumbImgs = []
         let data = this.props.wpData
         for (let i=0;i<data.length;i++) {
             if (data[i].tags[0] === 103) { // wp tag 'news'
                 posts.push(data[i].title.rendered)
                 posts.push(data[i].content.rendered)
                 slugs.push(data[i].slug)
-                //continue working from here
             }
         }
         this.setState({ 
@@ -37,23 +38,30 @@ class Home extends Component {
         }
         var thumbPosts = []
         var newsPosts = []
+        var thumbImgUrls = []
+        let imgArray = posts[1].match('data-orig-file=\\"https://(.*).jpg?')[0].split(' ')
+        let imgUrl = imgArray[imgArray.length-1]
+        let imgArray2 = posts[3].match('data-orig-file=\\"https://(.*).jpg?')[0].split(' ')
+        thumbImgUrls.push(imgArray2[imgArray2.length-1])
+        let imgArray3 = posts[5].match('data-orig-file=\\"https://(.*).jpg?')[0].split(' ')
+        thumbImgUrls.push(imgArray3[imgArray3.length-1])
+        let imgArray4 = posts[7].match('data-orig-file=\\"https://(.*).jpg?')[0].split(' ')
+        thumbImgUrls.push(imgArray4[imgArray4.length-1])
+        let imgArray5 = posts[9].match('data-orig-file=\\"https://(.*).jpg?')[0].split(' ')
+        thumbImgUrls.push(imgArray5[imgArray5.length-1])
         for (let i=0;i<4;i++){
-            thumbPosts.push({post: previousPosts[i], slug: previousSlugs[i]})
+            thumbPosts.push({post: previousPosts[i], slug: previousSlugs[i], img: thumbImgUrls[i]})
         }
-        for (let i=4;i<previousPosts.length;i++){
+        for (let i=4;i<16;i++){
             newsPosts.push({post: previousPosts[i], slug: previousSlugs[i]})
         }
-        this.setState({
+        this.setState({ 
+            bodyPosts: posts, 
+            featuredImg: imgUrl,
             thumbListObj: thumbPosts,
             newsListObj: newsPosts
         })
         for (let i=0;i<8;i++) { posts.push('') } // buffer 
-        let imgArray = posts[1].match('data-orig-file=\\"https://(.*).jpg?')[0].split(' ')
-        let imgUrl = imgArray[imgArray.length-1]
-        this.setState({ 
-            bodyPosts: posts, 
-            featuredImg: imgUrl
-        })
     }
     render() {  
         if (!this.state.bodyPosts)
@@ -78,8 +86,7 @@ class Home extends Component {
                                             <div className='thumbTitle'>
                                                 <h3 key={i} dangerouslySetInnerHTML={{ __html: slug.post}}/>
                                             </div>
-                                            <div className='thumbImg'>
-                                            </div>
+                                            <img key={i} src={slug.img} className='thumbImg' alt='thumbImage'/>
                                         </Link>
                                     )
                                 })
@@ -98,6 +105,17 @@ class Home extends Component {
                                     )
                                 })
                             }
+                        </div>
+                        <div className='twitterBox'>
+                            <img src={hr3} alt='-----'/>
+                            <a className='twitterBoxLink' href='http://eepurl.com/tEQzX'>
+                                <img className='twitterBoxLinkSrc' src={mailinglist} alt='OK Mailing List'/>
+                                <div className='tBoxHoverCover'/>
+                            </a>
+                            <a className='twitterBoxLink' href='http://twitter.com/oktheater'>
+                                <img className='twitterBoxLinkSrc' src={oktwitter} alt='OK Twitter'/>
+                                <div className='tBoxHoverCover'/>
+                            </a>
                         </div>
                     </div>
                 </div>
