6bb77df4ea9ed95bc80402927cbdd2865626cf1d
dhg <gibsonhdrew@gmail.com>
2017-12-19 16:44:44 -0500

    kafka landing page

--- src/modules/Splash.js
+++ src/modules/Splash.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import kafka from '../images/kafka.png'
 
 
 class Splash extends Component {
     render() {  
         return (
-            <div className='fades'>
-              <p>Splash page goes here!</p>
-              <Link to='/news'><button>Enter</button></Link>
+            <div className='fades' style={{
+              height:'100vh',
+              display: 'flex',
+              justifyContent: 'center'
+            }}>
+              <Link to='/news'>
+                <img src={kafka} alt='kafka' className='kafka' style={{
+                  width: '55vw',
+                  marginTop: '40px',
+                  zIndex:'5000',
+                  boxShadow: '3px 3px 3px #ECECEC',
+                }}/>
+              </Link>
+              <br/>
             </div>
         )
     }
