fd5f785fd83076cbf96b3f83d4126daef225201a
dhg <gibsonhdrew@gmail.com>
2018-02-06 17:11:34 -0500

    changed about pic

--- src/modules/Weareok.js
+++ src/modules/Weareok.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import about from '../images/about.jpg';
+import CrossfadeImage from 'react-crossfade-image';
 import wereok from '../images/pagetitles/wereok.png';
+import weareok1 from '../images/weareok/weareok1.jpg';
+import weareok2 from '../images/weareok/weareok2.jpg';
+import weareok3 from '../images/weareok/weareok3.jpg';
+import weareok4 from '../images/weareok/weareok4.jpg';
 import hr2 from '../images/hr2.png';
 import hr3 from '../images/hr3.png';
 
@@ -10,7 +14,9 @@ class Weareok extends Component {
             <div className='aboutBody fades'>
                 <img src={wereok} alt='We&#8217re OK' className='pageTitle'/>
                 <br/><br/>
-                <img src={about} alt='about' className='aboutPic'/>
+                <div className='imgShuffle'>
+                  <img src={weareok1} alt='about' className='aboutPic'/>
+                </div>
                 <p>Nature Theater of Oklahoma is an award-winning New York art and performance enterprise under the direction of Pavol Liska and Kelly Copper.  With each new project, we attempt to set an impossible challenge for ourselves, the audience, and our collaborators -- working from inside the codes and confines of established genres and exploding them. No two projects are formally the same, but the work is always full of humor, earnestness, rigor, and the audience plays an essential role -- whether as spectators or - just as often - as participants in the work.</p>
                 <p>Using readymade material, found space, gifted properties, cosmic accident, extreme formal manipulation and plain hard work -- Nature Theater of Oklahoma makes art to affect a shift in the perception of everyday reality that extends beyond the site of performance and into the world in which we live.</p>
                 <div className='aboutHrs'>
