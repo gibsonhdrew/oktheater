22fac92a5884850c82df417f1f365fa17dc295f3
dhg <gibsonhdrew@gmail.com>
2018-02-10 15:58:15 -0500

    fades between four images on we are ok

--- src/modules/Weareok.js
+++ src/modules/Weareok.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import CrossfadeImage from 'react-crossfade-image';
 import wereok from '../images/pagetitles/wereok.png';
 import weareok1 from '../images/weareok/weareok1.jpg';
 import weareok2 from '../images/weareok/weareok2.jpg';
@@ -14,9 +13,23 @@ class Weareok extends Component {
             <div className='aboutBody fades'>
                 <img src={wereok} alt='We&#8217re OK' className='pageTitle'/>
                 <br/><br/>
-                <div className='imgShuffle'>
-                  <img src={weareok1} alt='about' className='aboutPic'/>
-                  <p style={{fontSize: '10px', marginBottom:'20px'}}>Photo credit: Ditz Fejer</p>
+                <div className='slider'>
+									<div className='slide1'>
+											<img src={weareok1} alt='about' className='aboutPic'/>
+											<p style={{fontSize: '10px', marginTop: '2px', marginBottom:'20px'}}>Photo credit: Ditz Fejer</p>
+									</div>
+									<div className='slide2'>
+											<img src={weareok2} alt='about' className='aboutPic'/>
+											<p style={{fontSize: '10px', marginTop: '2px', marginBottom:'20px'}}>Photo credit: Nature Theater of Oklahoma</p>
+									</div>
+									<div className='slide3'>
+											<img src={weareok3} alt='about' className='aboutPic'/>
+											<p style={{fontSize: '10px', marginTop: '2px', marginBottom:'20px'}}>Photo credit: Andrej Lamut</p>
+									</div>
+									<div className='slide4'>
+											<img src={weareok4} alt='about' className='aboutPic'/>
+											<p style={{fontSize: '10px', marginTop: '2px', marginBottom:'20px'}}>Photo credit: Nature Theater of Oklahoma</p>
+									</div>
                 </div>
                 <p>Nature Theater of Oklahoma is an award-winning New York art and performance enterprise under the direction of Pavol Liska and Kelly Copper.  With each new project, we attempt to set an impossible challenge for ourselves, the audience, and our collaborators -- working from inside the codes and confines of established genres and exploding them. No two projects are formally the same, but the work is always full of humor, earnestness, rigor, and the audience plays an essential role -- whether as spectators or - just as often - as participants in the work.</p>
                 <p>Using readymade material, found space, gifted properties, cosmic accident, extreme formal manipulation and plain hard work -- Nature Theater of Oklahoma makes art to affect a shift in the perception of everyday reality that extends beyond the site of performance and into the world in which we live.</p>
