b80c779d8914ed566cae04b90d0413a6f346c2f4
dhg <gibsonhdrew@gmail.com>
2018-07-27 20:10:54 -0400

    kafka pic animation

--- src/modules/Splash.js
+++ src/modules/Splash.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import hr4 from '../images/hr4.png'
+import kafka from '../images/kafka.jpg'
 
 
 class Splash extends Component {
@@ -12,6 +13,8 @@ class Splash extends Component {
             }}>
             <div className='splashBG' style={{
               backgroundColor:'#F6F6F6',
+              backgroundImage:`url(${kafka})`,
+              backgroundRepeat:'repeat',
               width:'100vw',
               opacity:'0.99',
               zIndex:'101',
@@ -23,10 +26,7 @@ class Splash extends Component {
                 <div className='kafka fades' style={{
                   marginTop: '30px',
                   zIndex:'5000',
-                  backgroundColor:'white',
-                  boxShadow: '3px 3px 3px #EFEFEF',
-                  opacity:'0.9',
-                  borderRadius: '2px',
+                  backgroundColor:'white'
                 }}>
                   <p>
                     &quot;Personnel is being hired for the Theater in Oklahoma! The Great Nature Theater of Oklahoma is calling you! It&#39;s calling you today only! If you miss this opportunity, there will never be another! Anyone thinking of his future, your place is with us! All welcome! Anyone who wants to be an artist, step forward! We are the theater that has a place for everyone, everyone in his place! If you decide to join us, we congratulate you here and now! But hurry, be sure not to miss the midnight deadline! We shut down at midnight, never to reopen! Accursed be anyone who doesn&#39;t believe us!&quot;
