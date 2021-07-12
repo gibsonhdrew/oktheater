886a9d1e49e8a4a19464373001bfbd9589c68481
gibsonhdrew <gibsonhdrew@gmail.com>
2021-07-11 20:48:29 -0400

    Fix image-loading issue in okt page

--- src/modules/Oktheater.js
+++ src/modules/Oktheater.js
@@ -1,105 +1,126 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import oktheater from '../images/pagetitles/oktheater.png';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import oktheater from "../images/pagetitles/oktheater.png";
 
 class Oktheater extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    var postTitles = [];
+    var postImages = [];
+    var slugs = [];
+    var currentPosts = [];
+    var previousPosts = [];
+    var previousSlugs = [];
+    let data = this.props.wpData;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].tags[0] === 14694) {
+        // wp tag 'theater'
+        postTitles.push(data[i].title.rendered);
+        slugs.push(data[i].slug);
+        let imgArray = data[i].content.rendered
+          .match('data-orig-file=\\"https://(.*).jpg')[0]
+          .split(" ");
+        postImages.push(imgArray[imgArray.length - 1].replace('src="', ""));
+      }
     }
-    componentDidMount() {
-        var postTitles = []
-        var postImages = []
-        var slugs = []
-        var currentPosts = []
-        var previousPosts = []
-        var previousSlugs = []
-        let data = this.props.wpData
-        for (let i=0;i<data.length;i++) {
-            if (data[i].tags[0] === 14694) { // wp tag 'theater'
-                postTitles.push(data[i].title.rendered)
-                slugs.push(data[i].slug)
-                let imgArray = data[i].content.rendered.match('data-orig-file=\\"https://(.*).jpg')[0].split(' ')
-                postImages.push(imgArray[imgArray.length-1])
-            }
-        }
-        for (let i=0;i<postImages.length;i++){
-            currentPosts.push({title: postTitles[i], img: postImages[i], url: slugs[i]})
-        }
-        if (postTitles.length > 4) {
-            for (let i=16;i<postTitles.length;i++){
-                previousPosts.push(postTitles[i])
-            }
-            for (let i=16;i<slugs.length;i++){
-                previousSlugs.push(slugs[i])
-            }
-        }
-        var prevPosts = []
-        for (let i=0;i<previousPosts.length;i++){
-            prevPosts.push({post: previousPosts[i], slug: previousSlugs[i]})
-        }
-        this.setState({ 
-            currentPosts: currentPosts, 
-            previousPostObj: prevPosts
-        })
+    for (let i = 0; i < postImages.length; i++) {
+      currentPosts.push({
+        title: postTitles[i],
+        img: postImages[i],
+        url: slugs[i],
+      });
     }
-    render() {  
-        if (!this.state.currentPosts)
-           return null;
+    if (postTitles.length > 4) {
+      for (let i = 16; i < postTitles.length; i++) {
+        previousPosts.push(postTitles[i]);
+      }
+      for (let i = 16; i < slugs.length; i++) {
+        previousSlugs.push(slugs[i]);
+      }
+    }
+    var prevPosts = [];
+    for (let i = 0; i < previousPosts.length; i++) {
+      prevPosts.push({ post: previousPosts[i], slug: previousSlugs[i] });
+    }
+    this.setState({
+      currentPosts: currentPosts,
+      previousPostObj: prevPosts,
+    });
+  }
+  render() {
+    if (!this.state.currentPosts) return null;
 
-        return (
-            <div className='oktBody fades'>
-                <img src={oktheater} alt='OK Theater' className='pageTitle'/>
+    return (
+      <div className="oktBody fades">
+        <img src={oktheater} alt="OK Theater" className="pageTitle" />
 
-                <div className='pageBody'>
-                    <div className='postGrid' style={{display:'flex',flexWrap:'wrap'}}>
-                        { 
-                            this.state.currentPosts.map(function(slug, i){
-                                return ( 
-                                    <div key={i} className='gridMap' style={{
-                                        backgroundColor:'white',
-                                        marginTop:'20px',
-                                        marginRight:'12px'
-                                    }}>
-                                      <Link key={i} to={'/oktheater/'+slug.url}>
-                                          <img src={slug.img} alt='Img' style={{
-                                              height: '100%',
-                                              objectFit:'cover'
-                                          }}/>
-                                          <p key={i} style={{
-                                              color:'white',
-                                              backgroundColor: '#222222',
-                                              width: 'calc(100% - 2px)',
-                                              opacity: '0.8',
-                                              marginTop: '-38px',
-                                              paddingTop: '8px',
-                                              height: '30px',
-                                              fontWeight:'bold'
-                                          }} dangerouslySetInnerHTML={{ __html: slug.title}}/>
-                                      </Link> 
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    <br/>
-                    <div>
-                        { 
-                            this.state.previousPostObj.map(function(slug, i){
-                                return ( 
-                                    <Link key={i} to={'/oktheater/'+slug.slug}>
-                                        <p className='previousLink' key={i} dangerouslySetInnerHTML={{ __html: slug.post}}/>
-                                    </Link> 
-                                )
-                            })
-                        }
-                    </div>
-                    <br/><br/>
+        <div className="pageBody">
+          <div
+            className="postGrid"
+            style={{ display: "flex", flexWrap: "wrap" }}
+          >
+            {this.state.currentPosts.map(function (slug, i) {
+              return (
+                <div
+                  key={i}
+                  className="gridMap"
+                  style={{
+                    backgroundColor: "white",
+                    marginTop: "20px",
+                    marginRight: "12px",
+                  }}
+                >
+                  <Link key={i} to={"/oktheater/" + slug.url}>
+                    <img
+                      src={slug.img}
+                      alt="Img"
+                      style={{
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    />
+                    <p
+                      key={i}
+                      style={{
+                        color: "white",
+                        backgroundColor: "#222222",
+                        width: "calc(100% - 2px)",
+                        opacity: "0.8",
+                        marginTop: "-38px",
+                        paddingTop: "8px",
+                        height: "30px",
+                        fontWeight: "bold",
+                      }}
+                      dangerouslySetInnerHTML={{ __html: slug.title }}
+                    />
+                  </Link>
                 </div>
-            </div>
-        )
-    }
+              );
+            })}
+          </div>
+          <br />
+          <div>
+            {this.state.previousPostObj.map(function (slug, i) {
+              return (
+                <Link key={i} to={"/oktheater/" + slug.slug}>
+                  <p
+                    className="previousLink"
+                    key={i}
+                    dangerouslySetInnerHTML={{ __html: slug.post }}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+          <br />
+          <br />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Oktheater;
